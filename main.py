773820e05220a4b0bc8aaf248bdc8288bfb384bd
Anthony Suárez <neolight1010@gmail.com>
2021-04-26 01:11:33 -0500

    Solve project's exercises (college related).

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ def main():
     matrix = np.array([[1, 3], [1, 4]])
     encoded = "15 16 58 71 61 81 45 54 18 23 76 95 57 71 40 53 30 37 7 9 27 32 91 115"
 
-    print(encode_string(string, matrix, only_alpha=True))
-    print(decode_string(encoded, matrix, alpha_only=True))
+    print(
+        f'Encode "Las matrices son amigables": {encode_string(string, matrix, only_alpha=True)}'
+    )
+    print()
+    print(
+        f"Decoding previous output: {decode_string(encoded, matrix, alpha_only=True)}"
+    )
+
+    print()
+    matrix = np.array([[1, -1, 0], [4, -2, 3], [2, 1, 5]])
+    encoded = "8 63 66 2 106 161 -2 1 10 -6 19 53 -6 96 180"
+    print(f"Decode {encoded}")
+    print(f"Decoded: {decode_string(encoded, matrix, alpha_only=True)}")
 
 
 if __name__ == "__main__":
