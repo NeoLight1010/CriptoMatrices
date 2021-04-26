3eb7e2600fbc252cef8e8be94f2c27e4feb95f97
Anthony Suárez <neolight1010@gmail.com>
2021-04-26 01:01:05 -0500

    Add decode function

---
+++ decode.py
@@ -0,0 +1,35 @@
+from constants import *
+import numpy as np
+
+
+def decode_string(
+    encoded: str, key_matrix: np.ndarray, alpha_only: bool = False
+) -> str:
+    """Decodes an encoded string of format "[X1] [X2] [X3] [...] [Xn]".
+
+    Assumes the given string has the correct n (n % key_matrix_n == 0)
+    """
+    matrix_n = key_matrix.shape[0]
+    encoded_values = [int(x) for x in encoded.split(" ")]
+
+    # Group numbers
+    value_groups = []
+    for i in range(0, len(encoded_values), matrix_n):
+        value_groups.append(np.array(encoded_values[i : i + matrix_n]))
+
+    # Get decoded matrix
+    matrix_inv = np.linalg.inv(key_matrix)
+    decoded_matrices = []
+    for group in value_groups:
+        t = group.T
+        decoded_group = np.dot(matrix_inv, t).T
+        decoded_matrices.append(decoded_group)
+
+    # Generate decoded string
+    gap = ALPHA_GAP if alpha_only else 0
+    decoded_str = ""
+    for group in decoded_matrices:
+        for element in group:
+            decoded_str = decoded_str + chr(int(element + gap))
+
+    return decoded_str
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from decode import decode_string
 import numpy as np
 from encode import encode_string
 
@@ -5,8 +6,10 @@ from encode import encode_string
 def main():
     string = "Las matrices son amigables"
     matrix = np.array([[1, 3], [1, 4]])
+    encoded = "15 16 58 71 61 81 45 54 18 23 76 95 57 71 40 53 30 37 7 9 27 32 91 115"
 
     print(encode_string(string, matrix, only_alpha=True))
+    print(decode_string(encoded, matrix, alpha_only=True))
 
 
 if __name__ == "__main__":
